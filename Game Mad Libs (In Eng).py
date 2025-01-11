d5e3d8ecbe2aad8797356918a83c1a3cc5c29c3d
Nikita <nperveytalov7@gmail.com>
2025-01-11 22:28:43 +0300

    Add files via upload

---
+++ Game Mad Libs (In Eng).py
@@ -0,0 +1,10 @@
+def mad_libs():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    adverb = input("Enter an adverb: ")
+
+    print("Once upon a time, a", adjective, noun)
+    print("decided to", verb, adverb, "in the forest.")
+
+mad_libs()
---
+++ Game Mad Libs (На Рус).py
@@ -0,0 +1,12 @@
+def mad_libs():
+    noun = input('Введите существительное: ')
+    verb = input('Введите глагол: ')
+    adjective = input('Введите прилагательное: ')
+    adverb = input('Введите наречие:  ')
+
+
+    print('_________________________________')
+    print('Однажды', adjective, noun)
+    print('Решил(-а)', verb, adverb, 'в лесу')
+
+mad_libs()
